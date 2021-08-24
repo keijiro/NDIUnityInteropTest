dd8b0c111cf255db7a34cc70c0606e36a0c7cf8d
Keijiro Takahashi <keijiro@gmail.com>
2021-08-24 17:44:18 +0900

    NDI 5 test

--- jp.keijiro.klak.ndi/Runtime/Interop/Config.cs
+++ jp.keijiro.klak.ndi/Runtime/Interop/Config.cs
@@ -5,7 +5,7 @@ namespace Klak.Ndi.Interop
     #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
         public const string DllName = "Processing.NDI.Lib.x64";
     #elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-        public const string DllName = "libndi.4";
+        public const string DllName = "libndi";
     #elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
         public const string DllName = "ndi";
     #else
