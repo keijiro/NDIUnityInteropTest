b3285c8a4b0b424b635b93c6915703c28088dbbc
Keijiro Takahashi <keijiro@gmail.com>
2021-09-09 13:28:22 +0900

    Cosmetic changes

--- jp.keijiro.klak.ndi/Runtime/Internal/Utility.cs
+++ jp.keijiro.klak.ndi/Runtime/Internal/Utility.cs
@@ -3,38 +3,40 @@ using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using IntPtr = System.IntPtr;
 
-namespace Klak.Ndi
+namespace Klak.Ndi {
+
+// Small utility functions
+static class Util
 {
-    static class Util
+    public static int FrameDataSize(int width, int height, bool alpha)
+      => width * height * (alpha ? 3 : 2);
+
+    public static bool CheckAlpha(Interop.FourCC fourCC)
+      => fourCC == Interop.FourCC.UYVA;
+}
+
+// Extension method to add IntPtr support to ComputeBuffer.SetData
+static class ComputeBufferExtension
+{
+    public unsafe static void SetData
+      (this ComputeBuffer buffer, IntPtr pointer, int count, int stride)
     {
-        public static int FrameDataSize(int width, int height, bool alpha)
-          => width * height * (alpha ? 3 : 2);
+        // NativeArray view for the unmanaged memory block
+        var view =
+          NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<byte>
+            ((void*)pointer, count * stride, Allocator.None);
 
-        public static bool CheckAlpha(Interop.FourCC fourCC)
-          => fourCC == Interop.FourCC.UYVA;
-    }
+        #if ENABLE_UNITY_COLLECTIONS_CHECKS
+        var safety = AtomicSafetyHandle.Create();
+        NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref view, safety);
+        #endif
 
-    // Extension method to add IntPtr support to ComputeBuffer.SetData
-    static class ComputeBufferExtension
-    {
-        public unsafe static void SetData
-          (this ComputeBuffer buffer, IntPtr pointer, int count, int stride)
-        {
-            // NativeArray view for the unmanaged memory block
-            var view =
-              NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<byte>
-                ((void*)pointer, count * stride, Allocator.None);
+        buffer.SetData(view);
 
-            #if ENABLE_UNITY_COLLECTIONS_CHECKS
-            var safety = AtomicSafetyHandle.Create();
-            NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref view, safety);
-            #endif
-
-            buffer.SetData(view);
-
-            #if ENABLE_UNITY_COLLECTIONS_CHECKS
-            AtomicSafetyHandle.Release(safety);
-            #endif
-        }
+        #if ENABLE_UNITY_COLLECTIONS_CHECKS
+        AtomicSafetyHandle.Release(safety);
+        #endif
     }
 }
+
+} // namespace Klak.Ndi
